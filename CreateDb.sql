ee3fbd34f8dfc1c7adf3a3c45d73627c8e842c50
Mikhail Holubeu <mihanekmiha3105@gmail.com>
2022-07-01 14:54:48 +0300

    Update scripts of creating and inserting in DB

--- CreateDb.sql
+++ CreateDb.sql
@@ -5,6 +5,22 @@ go
 use ProductsDb
 
 go
+CREATE TABLE Towns (
+    Id int PRIMARY KEY  NOT NULL,
+    Name nvarchar(100),
+    Code int
+);
+
+go
+CREATE TABLE Brands (
+    Id int PRIMARY KEY  NOT NULL,
+    Name nvarchar(100),
+    Description nvarchar(100),
+	TownId int,
+  FOREIGN KEY (TownId) REFERENCES Towns (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+go
 CREATE TABLE ProductTypes (
     Id int PRIMARY KEY  NOT NULL,
     Name nvarchar(20)
@@ -16,5 +32,7 @@ CREATE TABLE Products (
     Name nvarchar(100),
     Quantity int,
     TypeId int,
-  FOREIGN KEY (TypeId) REFERENCES ProductTypes (Id) ON DELETE CASCADE ON UPDATE CASCADE
+	BrandId int,
+  FOREIGN KEY (TypeId) REFERENCES ProductTypes (Id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (BrandId) REFERENCES Brands (Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
--- InsertData.sql
+++ InsertData.sql
@@ -1,9 +1,19 @@
 use ProductsDb
 
+INSERT [dbo].[Towns] ([Id], [Name], [Code]) VALUES (CAST(1 AS Numeric(18, 0)), N'Minsk', CAST(242 AS Numeric(18, 0)))
+INSERT [dbo].[Towns] ([Id], [Name], [Code]) VALUES (CAST(2 AS Numeric(18, 0)), N'San Francisco', CAST(941 AS Numeric(18, 0)))
+INSERT [dbo].[Towns] ([Id], [Name], [Code]) VALUES (CAST(3 AS Numeric(18, 0)), N'Moscow', CAST(353 AS Numeric(18, 0)))
+
+INSERT [dbo].[Brands] ([Id], [Name], [Description], [TownId]) VALUES (CAST(1 AS Numeric(18, 0)), N'Milkavita', N'Products from milk', CAST(1 AS Numeric(18, 0)))
+INSERT [dbo].[Brands] ([Id], [Name], [Description], [TownId]) VALUES (CAST(2 AS Numeric(18, 0)), N'Apple', N'Electronics manufacture', CAST(2 AS Numeric(18, 0)))
+INSERT [dbo].[Brands] ([Id], [Name], [Description], [TownId]) VALUES (CAST(3 AS Numeric(18, 0)), N'Luxardi', N'Build materials and other building staff', CAST(3 AS Numeric(18, 0)))
+
 INSERT [dbo].[ProductTypes] ([Id], [Name]) VALUES (CAST(1 AS Numeric(18, 0)), N'Food')
 INSERT [dbo].[ProductTypes] ([Id], [Name]) VALUES (CAST(2 AS Numeric(18, 0)), N'Drinks')
+INSERT [dbo].[ProductTypes] ([Id], [Name]) VALUES (CAST(3 AS Numeric(18, 0)), N'Smartphones')
+INSERT [dbo].[ProductTypes] ([Id], [Name]) VALUES (CAST(4 AS Numeric(18, 0)), N'Build materials')
 
-INSERT [dbo].[Products] ([Id], [Name], [Quantity], [TypeId]) VALUES (CAST(1 AS Numeric(18, 0)), N'Salad', CAST(25 AS Numeric(18, 0)), CAST(1 AS Numeric(18, 0)))
-INSERT [dbo].[Products] ([Id], [Name], [Quantity], [TypeId]) VALUES (CAST(2 AS Numeric(18, 0)), N'Cheese', CAST(19 AS Numeric(18, 0)), CAST(1 AS Numeric(18, 0)))
-INSERT [dbo].[Products] ([Id], [Name], [Quantity], [TypeId]) VALUES (CAST(3 AS Numeric(18, 0)), N'Coffee', CAST(36 AS Numeric(18, 0)), CAST(2 AS Numeric(18, 0)))
-INSERT [dbo].[Products] ([Id], [Name], [Quantity], [TypeId]) VALUES (CAST(4 AS Numeric(18, 0)), N'Tea', CAST(14 AS Numeric(18, 0)), CAST(2 AS Numeric(18, 0)))
+INSERT [dbo].[Products] ([Id], [Name], [Quantity], [TypeId], [BrandId]) VALUES (CAST(1 AS Numeric(18, 0)), N'Milk', CAST(50 AS Numeric(18, 0)), CAST(2 AS Numeric(18, 0)), CAST(1 AS Numeric(18, 0)))
+INSERT [dbo].[Products] ([Id], [Name], [Quantity], [TypeId], [BrandId]) VALUES (CAST(2 AS Numeric(18, 0)), N'Cheese', CAST(100 AS Numeric(18, 0)), CAST(1 AS Numeric(18, 0)), CAST(1 AS Numeric(18, 0)))
+INSERT [dbo].[Products] ([Id], [Name], [Quantity], [TypeId], [BrandId]) VALUES (CAST(3 AS Numeric(18, 0)), N'iPhone 13', CAST(82 AS Numeric(18, 0)), CAST(3 AS Numeric(18, 0)), CAST(2 AS Numeric(18, 0)))
+INSERT [dbo].[Products] ([Id], [Name], [Quantity], [TypeId], [BrandId]) VALUES (CAST(4 AS Numeric(18, 0)), N'Brick', CAST(1400 AS Numeric(18, 0)), CAST(4 AS Numeric(18, 0)), CAST(3 AS Numeric(18, 0)))
